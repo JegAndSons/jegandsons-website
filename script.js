f61e91578f9b774d81aa2dbfd7e4cb76c0007b4b
JA Balbin <swengr.johnbalbin@gmail.com>
2025-02-01 02:17:34 +0800

    fix mobile responsiveness

--- script.js
+++ script.js
@@ -72,3 +72,12 @@ document.querySelectorAll(".stats-status").forEach((el) => {
   const isIncrement = status === "increment";
   animatePercentage(el, 0, targetPercentage, 3000, isIncrement);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", function () {
+      navLinks.classList.toggle("active");
+  });
+});
